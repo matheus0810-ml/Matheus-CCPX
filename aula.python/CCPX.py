300a70f5ff23fe4447bb24ce88d3fe5411116477
matheus0810-ml <matheuscmoreno2007@gmail.com>
2026-03-19 22:28:58 -0300

    Versão: aula 2, variaveis e operadores

---
+++ aula.python/CCPX.py
@@ -0,0 +1,19 @@
+print('Hello, World!')
+
+nome= "Matheus" # str
+idade= 18 #int
+peso= 62 #float
+
+print (nome, idade, peso)
+print (f'Oi {nome}!!!')
+
+nome= input('Digite o seu nome: \n')
+idade= input('Digite sua idade: \n')
+
+nova_idade= idade+1
+
+print(nome, idade)
+print(nova_idade)
+
+
+
